7d1ad7dac06d6bddd06abff79cefccc2dbf9f3ee
juanjo-lg <juanjo_lg@hotmail.es>
2020-05-20 20:44:42 +0200

    base de partida

---
+++ scripts/main.js
@@ -0,0 +1,28 @@
+/* Script para WEB de Curriculum Vitae */
+
+
+
+var miImage = document.querySelector('img');
+ miImage.onclick = function () {
+     var miSrc = miImage.getAttribute('src');
+     if (miSrc === 'D:/WEB/CV/images/foto_carnet.jpg') {
+         miImage.setAttribute('src', 'D:/WEB/CV/images/test.png');
+     } else {
+         miImage.setAttribute('src', 'D:/WEB/CV/images/foto_carnet.jpg');
+     } 
+ }
+
+var miBoton = document.querySelector('button');
+var miTitulo = document.querySelector('h1')
+
+
+// No consigo hacer funcionar el localStorage.
+function nombreUsuario() {
+    var miNombre = prompt('Por favor, ingrese su nombre.');
+    miTitulo.textContent = '¡Bienvenido,' + ' ' + miNombre + '!';
+    localStorage.setItem('nombre', miNombre);
+}
+
+miBoton.onclick = function() {
+    nombreUsuario();
+}
