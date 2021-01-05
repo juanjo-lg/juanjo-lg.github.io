16432dec6edd69f842b3ade90df675a0c080fdcf
juanjo-lg <juanjo_lg@hotmail.es>
2021-01-05 09:11:12 +0100

    Add objects to 3d3 and handle3d

--- scripts/handle3d.js
+++ scripts/handle3d.js
@@ -1,24 +1,24 @@
 // Script manejador para modelos 3D de Talavera de la Reina.
 
-var muralla = {
+let muralla = {
   nombre: "Muralla",
   area: "25 m2",
   volumen: "47 m3"
 };
 
-var torre = {
+let torre = {
   nombre: "Torre",
   area: "21 m2",
   volumen "60 m3":
 };
 
-var puerta = {
+let puerta = {
   nombre: "Puerta",
   area: "27 m2",
   volumen: "30 m3"
 };
 
-var hotel = {
+let hotel = {
   nombre: "Hotel",
   area: "15 m2",
   volumen: "80 m3"
